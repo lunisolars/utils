087bb77f68389542c31abb04f36a4cac578906b8
Natron Kan <waterbeside@163.com>
2023-05-13 13:47:17 +0800

    export all

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,22 @@
-import { REGEX_PARSE, UNITS } from './constants'
+export { REGEX_PARSE, UNITS } from './constants'
 export { cache } from './cache'
+export {
+  prettyUnit,
+  getTrigramValueByStem,
+  computeSBValue,
+  dateDict2jdms,
+  jdms2hms,
+  computeRatStem,
+  computeTriadE5Value,
+  computeGroup6E5Value
+} from './dateHelper'
 
-export { REGEX_PARSE, UNITS }
+export { toInt, hasProps, twoList2Dict, isNumber } from './func'
+export { getTranslation, defineLocale } from './lang'
+export {
+  getDefaultDateDict,
+  string2DateDict,
+  dateDict2Date,
+  dateDictYMD2Date,
+  parseDate
+} from './parseDate'
