439662d472d48481bcedab892c69d9126c1a69bd
Natron Kan <waterbeside@163.com>
2023-05-15 14:25:52 +0800

    test: 补充测试用例

--- __tests__/jd.test.ts
+++ __tests__/jd.test.ts
@@ -1,4 +1,11 @@
-import { jdn2DateDict, gre2jdn, jdn2timestamp, dateDict2jdms } from '../lib/jd'
+import {
+  jdn2DateDict,
+  gre2jdn,
+  jdn2timestamp,
+  dateDict2jdms,
+  jdDict2timestamp,
+  timestamp2jdDict
+} from '../lib/jd'
 import { DateDict } from '../types/types'
 
 describe('test gre2jdn()', () => {
@@ -17,12 +24,12 @@ describe('test gre2jdn()', () => {
     expect(jdn).toBe(2460078.7083333335)
     expect(jdms).toBe(46800000 - 8 * 60 * 60 * 1000)
     expect(jdn2timestamp(jdn)).toBe(new Date(2023, 4, 14, 13).valueOf())
+    expect(jdDict2timestamp({ jdn, jdms })).toBe(new Date(2023, 4, 14, 13).valueOf())
   })
 })
 
 describe('test jdn2DateDict()', () => {
   it('test jdn2DateDict 1', () => {
-    console.log()
     expect(jdn2DateDict(2460078.7083333335, false, 46800000 - 8 * 60 * 60 * 1000)).toEqual({
       year: 2023,
       month: 5,
@@ -31,6 +38,20 @@ describe('test jdn2DateDict()', () => {
       minute: 0,
       second: 0,
       millisecond: 0
+    })
+  })
+})
+
+describe('test jdDict2timestamp() and timestamp2jdDict', () => {
+  it('test jdDict2timestamp 1', () => {
+    const jdms = 46800000 - 8 * 60 * 60 * 1000
+    expect(jdDict2timestamp({ jdn: 2460078.708333, jdms })).toBe(1684040400000)
+  })
+
+  it('test timestamp2jdDict', () => {
+    expect(timestamp2jdDict(1684040400000)).toEqual({
+      jdn: 2460078.7083333335,
+      jdms: 46800000 - 8 * 60 * 60 * 1000
     })
   })
 })
